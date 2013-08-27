3c1fd3cee217aa042352b76fb30b1fbf3875117a
mqprichard <mqprichard@gmail>
2013-08-26 19:18:20 -0700

    Removed old ios-* targets

--- ios-clickstart/CBAppDelegate.h
+++
@@ -1,9 +0,0 @@
-@class CBViewController;
-
-@interface CBAppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
-@property (strong, nonatomic) CBViewController *viewController;
-
-@end
--- ios-clickstart/CBViewController.h
+++
@@ -1,8 +0,0 @@
-@interface CBViewController : UIViewController<UIPickerViewDataSource, UIPickerViewDelegate>
-
-- (NSString *)hello:(NSString *)name more:(NSString *)more;
-+ (void) registerWithPushServer:(NSString *)token;
-+ (void) showMessage:(NSString *)heading message:(NSString *)message;
-
-
-@end
